604c2fe391fbdcea4298b0d8d1076e3bbded3be9
Night6997 <83875915+Night6997@users.noreply.github.com>
2021-12-15 10:42:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+function startIdentification(){
+
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/Xv9JgGazE/model.json", model_ready); 
+
+}
+
+function model_ready(){
+
+    classifier.classify(getResults);
+
+}
+
+function getResults(error, result){
+
+    if(error){
+
+        console.error(error);
+
+    }
+    else{
+
+        console.log(result);
+        color_r=Math.floor(Math.random() * 255) + 1;
+        color_g=Math.floor(Math.random() * 255) + 1;
+        color_b=Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_Detected").innerHTML = ' '+ result[0].label; 
+        document.getElementById("result_accuracy").innerHTML = 'Accuracy - '+ (result[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_Detected").style.color="rgb("+color_r+","+color_g+","+color_b+")";
+        document.getElementById("result_Voice").style.color="rgb("+color_r+","+color_g+","+color_b+")";
+        document.getElementById("result_accuracy").style.color="rgb("+color_r+","+color_g+","+color_b+")";
+
+    }
+
+    if(result[0].label=="dog"){
+
+        document.getElementById("result_Voice").innerHTML="Dog"; //fix sentences//
+        document.getElementById("output").innerHTML='<img src="dog.jpg">';
+
+    }
+
+    if(result[0].label=="cat"){
+
+        document.getElementById("result_Voice").innerHTML="Cat";
+        document.getElementById("output").innerHTML='<img src="cat.jpg">';
+
+    }
+
+    if(result[0].label=="owl"){
+
+        document.getElementById("result_Voice").innerHTML="Owl";
+        document.getElementById("output").innerHTML='<img src="owl.jpg">';
+
+    }
+
+}
