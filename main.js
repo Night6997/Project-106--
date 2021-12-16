52ed3b3294d915fdd23eb27d24ccba03ac833215
Night6997 <83875915+Night6997@users.noreply.github.com>
2021-12-16 18:59:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,9 +25,7 @@ function getResults(error, result){
         color_g=Math.floor(Math.random() * 255) + 1;
         color_b=Math.floor(Math.random() * 255) + 1;
 
-        document.getElementById("result_Detected").innerHTML = ' '+ result[0].label; 
         document.getElementById("result_accuracy").innerHTML = 'Accuracy - '+ (result[0].confidence*100).toFixed(2)+" %";
-        document.getElementById("result_Detected").style.color="rgb("+color_r+","+color_g+","+color_b+")";
         document.getElementById("result_Voice").style.color="rgb("+color_r+","+color_g+","+color_b+")";
         document.getElementById("result_accuracy").style.color="rgb("+color_r+","+color_g+","+color_b+")";
 
@@ -35,21 +33,21 @@ function getResults(error, result){
 
     if(result[0].label=="dog"){
 
-        document.getElementById("result_Voice").innerHTML="Dog"; //fix sentences//
+        document.getElementById("result_Voice").innerHTML="Detected sound is a dog barking";
         document.getElementById("output").innerHTML='<img src="dog.jpg">';
 
     }
 
     if(result[0].label=="cat"){
 
-        document.getElementById("result_Voice").innerHTML="Cat";
+        document.getElementById("result_Voice").innerHTML="Detected sound is a cat meowing ";
         document.getElementById("output").innerHTML='<img src="cat.jpg">';
 
     }
 
     if(result[0].label=="owl"){
 
-        document.getElementById("result_Voice").innerHTML="Owl";
+        document.getElementById("result_Voice").innerHTML="Detected sound is a owl hooting";
         document.getElementById("output").innerHTML='<img src="owl.jpg">';
 
     }
